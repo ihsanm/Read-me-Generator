214aa0360fc76ef9571e642c51e49c0bfe80cfc7
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-27 14:05:27 +0000

    added questions to questions array

--- starter/index.js
+++ starter/index.js
@@ -4,9 +4,42 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
+    {
+        name:"Project-name",
+        type: "input",
+        message: "What is your projects name?"
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "give a description about your project"
+    },
+    {
+        name:"installation",
+        type: "input",
+        message: "what installation is required?"
+    },
+    {
+        name:"usage",
+        type: "input",
+        message: "what usage does your project have?"
+    },
+    {
+        name:"credits",
+        type: "input",
+        message: "give credits"
+    },
+    {
+        name:"License",
+        type: "input",
+        message: "what licenses do you use?"
+    }
 
-];
+])
+};
+questions()
 
 // function to write README file
 function writeToFile(fileName, data) {
